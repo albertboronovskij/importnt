19cdccf841d9c9ffd6a0bd6c4e54626791e9c1e0
albertboronovskij <albertikus1010@gmail.com>
2023-03-01 15:59:50 +0700

    сделал зд2 к 1 уроку

---
+++ seminars&homework/homework_1/ex2/Program.cs
@@ -0,0 +1,17 @@
+﻿int a=8;
+int b=5;
+
+if (b>a)
+{
+   Console.Write(b);
+   Console.WriteLine("=max");
+   Console.Write(a);
+   Console.Write("=min");
+}
+else
+{
+   Console.Write(b);
+   Console.WriteLine("=max");
+   Console.Write(a);
+   Console.Write("=min");
+}
---
+++ seminars&homework/homework_1/ex4/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] n={3, 6, 9,};
+int max=0;
+int[] i=0;
+while (i<3)
+{
+    bool v = n[i]>max;
+    i=i+1;
+}
+
+
+Console.WriteLine(max);
