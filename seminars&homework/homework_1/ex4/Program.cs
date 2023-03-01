100f5e32dc926c0c28b115a4793e72de0d6a1c6b
albertboronovskij <albertikus1010@gmail.com>
2023-03-01 17:47:58 +0700

    do ex4

--- seminars&homework/homework_1/ex4/Program.cs
+++ seminars&homework/homework_1/ex4/Program.cs
@@ -1,11 +1,35 @@
-﻿int[] n={3, 6, 9,};
-int max=0;
-int[] i=0;
-while (i<3)
+﻿
 {
-    bool v = n[i]>max;
-    i=i+1;
-}
+ int max;
+ int a=4;
+ int b=3;
+ int c=1998;       
+        if (a > b)
+        {
+            if (a > c)
+            {
+                max = a;
+            }
+            else
+            {
+                max = c;
+            }
+        }
+        else
+        {
+            if (b > c)
+            {
+                max = b;
+            }
+            else
+            {
+                max = c;
+            }
+        }
+        Console.WriteLine(max);
+    }
 
 
-Console.WriteLine(max);
+
+   
+
