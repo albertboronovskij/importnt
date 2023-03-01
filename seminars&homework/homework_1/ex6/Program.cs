764fd8d8b641d806e47e893e106c518a1a5cbd51
albertboronovskij <albertikus1010@gmail.com>
2023-03-01 19:18:19 +0700

    выполнил   ex8

---
+++ seminars&homework/homework_1/ex6/Program.cs
@@ -0,0 +1,12 @@
+﻿int n=6;
+
+
+        if (n % 2 == 0)
+        {
+            Console.WriteLine("переменная  кратна двум");
+        }
+        else
+        {
+            Console.WriteLine("переменная не кратна двум");
+        }
+
---
+++ seminars&homework/homework_1/ex8/Program.cs
@@ -0,0 +1,13 @@
+﻿int i=1;
+int n=29;
+        while (i <= n)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+            i = i + 1;
+        }
+    
+    
+    
