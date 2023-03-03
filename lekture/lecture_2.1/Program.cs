be1ccea1053232666601abc80e0843dda907e547
albertboronovskij <albertikus1010@gmail.com>
2023-03-03 16:38:31 +0700

    creat lessen 2.2

---
+++ lekture/lecture_2.1/Program.cs
@@ -0,0 +1,15 @@
+﻿int []array={11,12,13,14,15,16,16,18,19,10};
+
+int n= array.Length; 
+int find =16;
+
+int index=0;
+
+while (index<n)
+{
+    if (array[index]==find){
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
