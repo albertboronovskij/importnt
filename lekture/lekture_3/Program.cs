554412bf43d72b85f25ae6360f73884bc9c03cbb
albertboronovskij <albertikus1010@gmail.com>
2023-03-08 17:46:30 +0700

    be

--- lekture/lekture_3/Program.cs
+++ lekture/lekture_3/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] arr = {4,6,3,5,1,7,2,9,8};
+void PrintArray ( int[] array)
+{
+    int count= array.Length;
+for (int i = 0; i < count; i++)
+{
+ Console.Write($"{array[i]}");
+
+}
+Console.WriteLine ();
+}
+PrintArray(arr);
+void SelectionSort(int[]array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+    int minpos=i;
+for (int j = i  ; j< array[minpos]; j++)
+{
+    if (array[j]<array[minpos]);
+}
+
+     
+    int temporary=array[i];
+    array[i]=array[minpos];
+    array[minpos]=temporary; 
+
+}
+}
+PrintArray(arr);
+SelectionSort();
--- seminars&homework/homework_3/ex1/Program.cs
+++ seminars&homework/homework_3/ex1/Program.cs
@@ -1,22 +1,56 @@
-﻿
-int a = Console.ReadLine();
- if (GetFourRank(a)== GetFirstRank(a))
+﻿int a=12361;
+int GetFirstRank(int a)
 {
-if (GetThirdRank(a)== GetFiveRank(a))
+while (a>9)
 {
-      Console.WriteLine("это число полиндром");
+    a/=10;
+}
+return a%10;
+}
+int GetSecondRank(int a)
+{
+while (a>99)
+{
+    a/=10;
+}
+return a%10;
+}
+int GetFourRank(int a)
+{
+while (a>9999)
+{
+    a/=10;
+}
+return a%10;
+}
+int GetFiveRank(int a)
+{
+while (a>99999)
+{
+    a/=10;
+}
+return a%10;
+}
+int b=GetFirstRank(a);
+int c=GetSecondRank(a);
+int d=GetFourRank(a);
+int e=GetFiveRank(a);
+
+if (b==e&c==d)
+ //(GetFirstRank(a)==(GetFiveRank(a)));(GetSecondRank(a)==(GetFourRank(a) ));
+{
+    Console.WriteLine("это полиндром");
 
 
-}
 
-else
-{
-    Console.WriteLine("это число не полиндром");    
-}
-}
-else
-{
-    Console.WriteLine("это число не полиндром");
     
 }
-Console.WriteLine(GetThirdRank (a));
+
+else
+{
+    Console.WriteLine("это He полиндром");
+}
+//(GetFirstRank(a));
+//Console.Writeline(GetSecondRank(a));
+//Console.Writeline(GetFourRank(a));
+//Console.Writeline(GetFiveRank(a));
