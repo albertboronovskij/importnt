b805740670cc28df2d4e4ca7c42a926d0be1969a
albertboronovskij <albertikus1010@gmail.com>
2023-03-08 18:33:07 +0700

    creat hw ex3

---
+++ seminars&homework/homework_3/ex3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+        int a=1;
+        int m=10;
+        
+       
+        while (a <= m)
+        {
+            int t;
+            
+            t = a;
+            t = t * t * t;
+            Console.WriteLine(t);
+            a = a + 1;
+        }
+    
